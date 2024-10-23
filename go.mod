daca14c563dcb1acd16e118147c8ad07b199e6e7
puiki <puikic@163.com>
2024-10-23 20:47:17 +0800

    update

--- go.mod
+++ go.mod
@@ -4,10 +4,12 @@ go 1.22
 
 require (
 	github.com/gin-gonic/gin v1.10.0
+	github.com/go-sql-driver/mysql v1.8.1
 	github.com/jmoiron/sqlx v1.4.0
 )
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/bytedance/sonic v1.12.2 // indirect
 	github.com/bytedance/sonic/loader v0.2.0 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
--- src/sqlx/main.go
+++ src/sqlx/main.go
@@ -1,16 +1,130 @@
 package main
 
 import (
+	"database/sql/driver"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 )
 
 var db *sqlx.DB
 
+type user struct {
+	ID   int    `db:"id"`
+	Age  int    `db:"age"`
+	Name string `db:"name"`
+}
+
+func (u user) Value() (driver.Value, error) {
+	return []interface{}{u.Name, u.Age}, nil
+}
+
+// BatchInsertUsers2 使用sqlx.In帮我们拼接语句和参数, 注意传入的参数是[]interface{}
+func BatchInsertUsers2(users []interface{}) error {
+	query, args, _ := sqlx.In(
+		"INSERT INTO user (name, age) VALUES (?), (?), (?)",
+		users..., // 如果arg实现了 driver.Valuer, sqlx.In 会通过调用 Value()来展开它
+	)
+	fmt.Println(query) // 查看生成的querystring
+	fmt.Println(args)  // 查看生成的args
+	_, err := db.Exec(query, args...)
+	return err
+}
+
 func initDB() (err error) {
+	//CREATE DATABASE sql_demo;
+
+	//CREATE TABLE `user` (
+	//	`id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+	//	`name` VARCHAR(20) DEFAULT '',
+	//	`age` INT(11) DEFAULT '0',
+	//	PRIMARY KEY(`id`)
+	//)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
 	dsn := "root:cpq123.com@tcp(127.0.0.1:3306)/sql_demo?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := sqlx.Connect("mysql", dsn)
+	db, err = sqlx.Connect("mysql", dsn)
+	if err != nil {
+		fmt.Printf("connect err:%v\n", err)
+		return
+	}
+
+	return
+}
+
+// 插入数据
+func insertRowDemo() {
+	sqlStr := "insert into user(name, age) values (?,?)"
+	ret, err := db.Exec(sqlStr, "aaa", 1)
+	if err != nil {
+		fmt.Printf("insert failed, err:%v\n", err)
+		return
+	}
+	theID, err := ret.LastInsertId() // 新插入数据的id
+	if err != nil {
+		fmt.Printf("get lastinsert ID failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("insert success, the id is %d.\n", theID)
+}
+
+// 查询单条数据示例
+func queryRowDemo() {
+	sqlStr := "select id, name, age from user where id=?"
+	var u user
+	err := db.Get(&u, sqlStr, 1)
+	if err != nil {
+		fmt.Printf("get failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("id:%d name:%s age:%d\n", u.ID, u.Name, u.Age)
+}
+
+// 查询多条数据示例
+func queryMultiRowDemo() {
+	sqlStr := "select id, name, age from user where id > ?"
+	var users []user
+	err := db.Select(&users, sqlStr, 0)
+	if err != nil {
+		fmt.Printf("query failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("users:%#v\n", users)
+}
+func insertUserDemo() (err error) {
+	sqlStr := "INSERT INTO user (name,age) VALUES (:name,:age)"
+	_, err = db.NamedExec(sqlStr,
+		map[string]interface{}{
+			"name": "七米",
+			"age":  28,
+		})
+	return
+}
+
+// 删除数据
+func deleteRowDemo() {
+	sqlStr := "delete from user where id = ?"
+	ret, err := db.Exec(sqlStr, 4)
+	if err != nil {
+		fmt.Printf("delete failed, err:%v\n", err)
+		return
+	}
+	n, err := ret.RowsAffected() // 操作影响的行数
+	if err != nil {
+		fmt.Printf("get RowsAffected failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("delete success, affected rows:%d\n", n)
 }
 func main() {
+	if err := initDB(); err != nil {
+		fmt.Printf("init err:%v\n", err)
+		return
+	}
+	fmt.Println("init success")
+	//insertRowDemo()
+	//queryRowDemo()
+	//queryMultiRowDemo()
+	//insertUserDemo()
+	//deleteRowDemo()
 
 }
