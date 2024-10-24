2225b96cf5908e64a141ee94894c2ea3b625f9f2
puiki <puikic@163.com>
2024-10-24 18:43:29 +0800

    update

--- src/go-redis/main.go
+++ src/go-redis/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "github.com/go-redis/redis"
+import (
+	"fmt"
+	"github.com/go-redis/redis"
+)
 
 var rdb *redis.Client
 
-func main() {}
+func initClient() (err error) {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	_, err = rdb.Ping().Result()
+	return err
+}
+func redisExample() {
+	err := rdb.Set("score", 100, 0).Err()
+	if err != nil {
+		fmt.Printf("set score failed, err:%v\n", err)
+		return
+	}
+
+	val, err := rdb.Get("score").Result()
+	if err != nil {
+		fmt.Printf("get score failed, err:%v\n", err)
+		return
+	}
+	fmt.Println("score", val)
+
+	val2, err := rdb.Get("testKey").Result()
+	if err == redis.Nil {
+		fmt.Println("name does not exist")
+	} else if err != nil {
+		fmt.Printf("get name failed, err:%v\n", err)
+		return
+	} else {
+		fmt.Println("name", val2)
+	}
+}
+
+func main() {
+	if err := initClient(); err != nil {
+		fmt.Printf("init redis client failed, err:%v\n", err)
+		return
+	}
+	fmt.Println("init redis client success")
+	defer rdb.Close()
+	redisExample()
+}
