5f3eebcf94c98cc21cc42fad4205e301788cc1dd
puiki <puikic@163.com>
2024-10-24 23:48:39 +0800

    update

--- src/zap/main.go
+++ src/zap/main.go
@@ -6,29 +6,35 @@ import (
 )
 
 var logger *zap.Logger
+var sugar *zap.SugaredLogger
 
 func initLogger() {
-	logger, _ = zap.NewProduction()
+	logger, _ = zap.NewDevelopment()
+	//logger, _ = zap.NewProduction()
+	sugar = logger.Sugar()
 }
+
 func simpleHttpGet(url string) {
 	resp, err := http.Get(url)
 	if err != nil {
-		logger.Error(
+		sugar.Error(
 			"Error fetching url",
-			zap.String("statusCode", resp.Status),
 			zap.String("url", url),
+			zap.Error(err),
 		)
 	} else {
-		logger.Info("Success...",
+		sugar.Info("Success...",
 			zap.String("statusCode", resp.Status),
 			zap.String("url", url),
 		)
 		resp.Body.Close()
 	}
 }
+
 func main() {
 	initLogger()
 	defer logger.Sync()
-	//simpleHttpGet("https://www.baidu.com")
+	simpleHttpGet("https://www.baidu.com")
 	simpleHttpGet("https://www.google.com")
+	//simpleHttpGet("https://www.sogo.com")
 }
