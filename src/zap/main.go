645490308377f21befb39032993ad629ff120a97
puiki <puikic@163.com>
2024-10-27 13:45:25 +0800

    update

--- src/zap/main.go
+++ src/zap/main.go
@@ -2,18 +2,34 @@ package main
 
 import (
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"net/http"
+	"os"
 )
 
 var logger *zap.Logger
 var sugar *zap.SugaredLogger
 
+//	func InitLogger() {
+//		logger, _ = zap.NewDevelopment()
+//		//logger, _ = zap.NewProduction()
+//		sugar = logger.Sugar()
+//	}
 func initLogger() {
-	logger, _ = zap.NewDevelopment()
-	//logger, _ = zap.NewProduction()
+	writeSyncer := getLogWriter()
+	encoder := getEncoder()
+	core := zapcore.NewCore(encoder, writeSyncer, zapcore.DebugLevel)
+	logger = zap.New(core)
 	sugar = logger.Sugar()
 }
+func getEncoder() zapcore.Encoder {
+	return zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
+}
 
+func getLogWriter() zapcore.WriteSyncer {
+	file, _ := os.Create("./test.log")
+	return zapcore.AddSync(file)
+}
 func simpleHttpGet(url string) {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -35,6 +51,6 @@ func main() {
 	initLogger()
 	defer logger.Sync()
 	simpleHttpGet("https://www.baidu.com")
-	simpleHttpGet("https://www.google.com")
+	//simpleHttpGet("https://www.google.com")
 	//simpleHttpGet("https://www.sogo.com")
 }
